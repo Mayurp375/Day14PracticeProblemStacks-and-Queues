5d61ec980f055766bf76666333d6917e15a8376e
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-20 20:54:23 +0530

    this is main file

---
+++ Main.java
@@ -0,0 +1,7 @@
+package classProblemByAmolMateSir.Day14PracticeProblem.StackAndQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to Stack and Queue");
+    }
+}
