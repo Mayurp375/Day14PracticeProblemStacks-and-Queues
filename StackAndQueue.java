db01f2bc15668950a6c257cd91faafcb218eb9f5
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-12-20 21:20:41 +0530

    Ability to add 54->30->70

---
+++ StackAndQueue.java
@@ -0,0 +1,57 @@
+package classProblemByAmolMateSir.Day14PracticeProblem.StackAndQueue;
+
+public class StackAndQueue<T> {
+
+    Node head;
+
+    class Node {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+
+        return head == null;
+    }
+
+    public void push(T data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = newNode;         //first element
+            return;
+        }
+        Node next = head;
+        head = newNode;
+    }
+
+    public T pop() {
+
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        }
+        T top = head.data;     //assinging last element value to top
+        head = head.next;
+        return top;
+
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        }
+        return head.data;
+
+    }
+
+    public static void main(String[] args) {
+        StackClass obj = new StackClass();
+        obj.push(56);
+        obj.push(30);
+        obj.push(70);
+    }
+}
